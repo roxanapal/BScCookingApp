ecc3d96d80fab2b48bb8beb9d8f7c6307b5656ad
RoxanaP <roxana.pal@devpadawan.ro>
2018-03-11 14:13:19 +0200

    Added layout for SignUp, added back button in toolbar.

--- app/src/main/java/com/easy/cooking/bsccookingapp/LoginActivity.java
+++ app/src/main/java/com/easy/cooking/bsccookingapp/LoginActivity.java
@@ -1,13 +1,39 @@
 package com.easy.cooking.bsccookingapp;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.tiet_login_username)
+    TextInputEditText etUsername;
+
+    @BindView(R.id.tiet_login_password)
+    TextInputEditText etPassword;
+
+    @BindView(R.id.tv_login_register)
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
