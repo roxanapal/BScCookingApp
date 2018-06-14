0d85d7d8e20e1fd0ebf5b85416763ee8ab8f5232
RoxanaP <roxana.pal@devpadawan.ro>
2018-06-15 00:42:15 +0300

    Added NavigationView, changed the colors of the app.

--- app/src/main/java/com/easy/cooking/learneat/MainActivity.java
+++ app/src/main/java/com/easy/cooking/learneat/MainActivity.java
@@ -1,7 +1,9 @@
 package com.easy.cooking.learneat;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.main_drawer_layout)
     DrawerLayout mainDrawerLayout;
 
+    @BindView(R.id.main_navigation_view)
+    NavigationView mainNavigationView;
+
     @BindView(R.id.main_rv_recipe_list)
     RecyclerView recyclerViewRecipe;
 
@@ -62,15 +67,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void setMenuFragment() {
-        menuFragment = new MenuFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_menu_container, menuFragment)
-                .commit();
+        setupDrawerContent(mainNavigationView);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, mainDrawerLayout, 0, 0);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
-
         mainDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mainDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
